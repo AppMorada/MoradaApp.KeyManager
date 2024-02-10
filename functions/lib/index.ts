e8d460dc354097124b5a7eb5ebd21488d40170ad
NicolasCBV <nicolascleiton700e@gmail.com>
2024-02-10 02:16:20 -0300

    ci(deploy-allfunctions.yml): adicionar pipeline de deploy geral
    adicionar pipeline de deploy geral com base nas modificações dos arquivos presentes em functions/lib

--- functions/lib/index.ts
+++ functions/lib/index.ts
@@ -1,19 +1,17 @@
-export * from "./storages/firestore/index"
-export * from "./storages/firestore/repositories/key"
+export * from './storages/firestore/index';
+export * from './storages/firestore/repositories/key';
 
-export * from "./utils/replace"
-export * from "./utils/response"
-export * from "./utils/httpHandler"
+export * from './utils/replace';
+export * from './utils/response';
+export * from './utils/httpHandler';
 
-export * from "./entities/key"
+export * from './entities/key';
+export * from './repositories/key';
 
-export * from "./repositories/key"
+export * from './config/logger/contract';
+export * from './config/logger/firebaseLogger/adapter';
 
-export * from "./config/logger/contract"
-export * from "./config/logger/firebaseLogger/adapter"
+export * from './errors/repositories/key';
+export * from './errors/firestore/index';
 
-export * from "./errors/repositories/key"
-export * from "./errors/firestore/index"
-
-export * from "./filters/index"
-
+export * from './filters/index';
